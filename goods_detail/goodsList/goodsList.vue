e5f7a7b3ad1fbcf94fdf4a52489056f8842eba08
cuidehua <1430344692@qq.com>
2022-07-16 21:39:43 +0800

    实现首页

---
+++ goods_detail/goodsList/goodsList.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		bababab
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
--- main.js
+++ main.js
@@ -2,10 +2,29 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import {$http} from "@escook/request-miniprogram"
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
+
+//把http挂载到uni上
+uni.$http=$http
+
+
+
+//拦截器进行加载的提示
+$http.beforeRequest = function(options){
+	uni.showLoading({
+		title:'数据加载中...'
+	})
+}
+
+$http.afterRequest = function(options){
+	uni.hideLoading()
+}
+
+
 
 const app = new Vue({
     ...App
